5ecfe7c4fdb128f64cdcbecb0c979eb443a33549
Cristina <kc.cristinadev@gmail.com>
2026-04-02 09:53:32 -0300

    Add files via upload

---
+++ catalogo/js/data.js
@@ -0,0 +1,58 @@
+export const categories = [
+    {
+        title: "Romance",
+        items: [
+                        {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTWGi4HO0YCfySfzzMbUVefOlaAN_yYUXsDrbrgAh081pNs-tZZzlkJVps&s=10", progress: 0,
+             nome: "Quase 18", youtube: "https://www.youtube.com/watch?v=3tV-C0eGIrQ"
+            },
+
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDEtmoVdlArinrN74R3ftB96Sd_jJRVnVDNw&s.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0, nome: "A Cinco Passos de Você",
+                youtube: "https://www.youtube.com/watch?v=IZefbFUlC14"
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8fcRJU3k1r3YyE1_dhWwUplIVlkHseBlOx7jLs80-6q1wfHshM2B-NGs&s=10", progress: 0,
+                nome: "Por Lugares Incríveis", 
+                youtube: "https://www.youtube.com/watch?v=tdwLRHGQVG8"
+            },
+            {
+                img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxL_bwjZp_ss5Jcct1kX-vRWEnEqt0KAIW22Wb0fl6vw&s=10", progress: 0,
+             nome: "Continência ao Amor", 
+                youtube: "https://www.youtube.com/watch?v=vUS1N24LoBc"
+            },
+
+        ]
+    },
+    {
+        title: "Terror",
+        items: [
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRK_1gbVjPO1CeC3kuf93gdFfqHFaLkaXp9cHqb9BjtnUJgDlcfEZRd5vw&s=10", top10: true, badge: "Nova temporada", badgeColor: "red",     
+            nome: "Pânico VI", 
+ youtube: "https://www.youtube.com/watch?v=QOgdGhA9gos" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCGApiFuDjKf4tlw32hH9r1WwtLQ6hY7qiZxzjRzVkSEAwmDSMOA59TICO&s=10", top10: true,
+                        nome: "Sorria", 
+youtube: "https://www.youtube.com/watch?v=MvJDb2GTxZA" },
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtKFDsoymnUQJwI9X4kwG9KUYjHowoER7hE1aede7CuZ2yEDxfr33x1EZS&s=10", badge: "Novo episódio", badgeColor: "red",
+                    nome: "A Morte Te Dá Parabéns", 
+youtube: "https://www.youtube.com/watch?v=Pxm5SfhomS8" },
+            { img: "https://m.media-amazon.com/images/S/pv-target-images/d1906aeb16983a097757c8339b39c116fb0a55af94b028919a0595d543f0d7e2.jpg", badge: "Novidade", badgeColor: "red", nome: "Não abra", 
+youtube: "https://www.youtube.com/watch?v=KH3hdqItCE8" },
+        ]
+    },
+    {
+        title: "Comedia",
+        items: [
+            { img: "https://akamai.sscdn.co/uploadfile/letras/playlists/8/6/7/b/867ba6f11ed8efa1725d117419bf86c5.jpg", top10: true,        nome: "As Branquelas", 
+youtube: "https://www.youtube.com/watch?v=seoJIPLLWp0" },
+          { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQC9kieq8sF5rKOh-W3aTjqG2iony0DbqJbZdnq--ebhzpFWQ7LYHfJLCsr&s=10", top10: true, badge: "Novidade", badgeColor: "red", nome: "Dançarina Imperfeita",  youtube: "https://www.youtube.com/watch?v=ayuqionmMts" },
+            { img: "https://i0.wp.com/cinestera.com.br/wp-content/uploads/2020/06/FF0D21A0-2EF3-45AD-BC6D-F40C4B8AAF69.jpeg?fit=1280%2C720&ssl=1", top10: true, badge: "Novidades", 
+            nome: "Feel the Beat",  
+            badgeColor: "red", youtube: "https://www.youtube.com/watch?v=hrzrcegbBEc" },
+            
+            { img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSFwgwBc8Dg019wUS8FUX_w1v-NCDr2nZAHDEBqMIZbZQ&s=10", top10: true, badge: "Novo episódio", badgeColor: "red",
+                    nome: "Esqueceram de Mim", 
+        youtube: "https://www.youtube.com/watch?v=Q2O2Ax8z35M" },
+        ]
+    }
+];
